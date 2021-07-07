dd3bd887ba61d081679073f594237cdac5981a74
abdulkareem-siddiq <73565559+abdulkareem-siddiq@users.noreply.github.com>
2021-07-06 17:24:32 -0700

    Create acw32h.hpp

---
+++ acw32h.hpp
@@ -0,0 +1,95 @@
+/*
+	Auto Closing Win32 HANDLE and HINTERNET wrappers
+
+	BSD 3-Clause License
+
+	Copyright (c) 2003-2020, Abdelkareem Siddiq
+	All rights reserved.
+
+	Redistribution and use in source and binary forms, with or without
+	modification, are permitted provided that the following conditions are met:
+
+	1. Redistributions of source code must retain the above copyright notice, this
+	list of conditions and the following disclaimer.
+
+	2. Redistributions in binary form must reproduce the above copyright notice,
+	this list of conditions and the following disclaimer in the documentation
+	and/or other materials provided with the distribution.
+
+	3. Neither the name of the copyright holder nor the names of its
+	contributors may be used to endorse or promote products derived from
+	this software without specific prior written permission.
+
+	THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+	AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+	IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+	DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+	FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+	DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+	SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+	CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+	OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+	OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#pragma once
+#ifndef ACW32HANDLE_HPP
+#define ACW32HANDLE_HPP
+
+#include <type_traits>
+
+/// @brief Automatically close the Win32 HANDLE or HINTERNET object
+/// @tparam T Must be either HANDLE or HINTERNET
+template <class T> struct ACW32H
+{
+	T _h {NULL};
+
+	ACW32H(T&& arg) noexcept
+	{
+#ifdef HINTERNET
+		if ((_h != NULL) && (_h != INVALID_HANDLE_VALUE) && std::is_same_v<T, HINTERNET>)
+		{
+			WinHttpCloseHandle(_h);
+		}
+#endif
+
+		if ((_h != NULL) && (_h != INVALID_HANDLE_VALUE) && std::is_same_v<T, HANDLE>)
+		{
+			CloseHandle(_h);
+		}
+		_h = std::exchange(arg, INVALID_HANDLE_VALUE);
+	}
+
+	~ACW32H() noexcept
+	{
+#ifdef HINTERNET
+		if ((_h != NULL) && (_h != INVALID_HANDLE_VALUE) && std::is_same_v<T, HINTERNET>) { WinHttpCloseHandle(_h); }
+#endif
+		if ((_h != NULL) && (_h != INVALID_HANDLE_VALUE) && std::is_same_v<T, HANDLE>) { CloseHandle(_h); }
+	}
+
+	ACW32H& operator=(T&& arg)
+	{
+#ifdef HINTERNET
+		if  ((_h != NULL) && (_h != INVALID_HANDLE_VALUE) && std::is_same_v<T, HINTERNET>)
+		{
+			WinHttpCloseHandle(_h);
+		}
+#endif
+
+		if  ((_h != NULL) && (_h != INVALID_HANDLE_VALUE) && std::is_same_v<T, HANDLE>)
+		{
+			CloseHandle(_h);
+		}
+		_h = std::exchange(arg, INVALID_HANDLE_VALUE);
+		return *this;
+	}
+
+	constexpr operator T() noexcept { return _h; }
+	constexpr operator bool() { return ((_h != NULL) && (_h != INVALID_HANDLE_VALUE)); }
+};
+
+
+using ACW32HANDLE = ACW32H<HANDLE>;
+
+#endif
